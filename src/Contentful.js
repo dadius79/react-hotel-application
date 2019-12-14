7150620f75a555f084c6ff8bd7ba61de99ba3a79
dadius79 <david.sule@outlook.com>
2019-12-15 02:49:18 +0300

    first commit

---
+++ src/Contentful.js
@@ -0,0 +1,7 @@
+//npm install contentful
+import { createClient } from "contentful";
+
+export default createClient({
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+});
